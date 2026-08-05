f794483e4d337d004544b608bcf43e81e989fe67
rrjackson82 <danonator821@gmail.com>
2026-08-04 21:06:22 -0400

    set up bot and added help command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::{prelude::*, utils::command::BotCommands};
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    log::info!("Starting bot");
+
+    let bot = Bot::from_env();
+
+    Command::repl(bot, answer).await;
+}
+
+// The 'comment' is actually what displays to the user
+#[derive(BotCommands, Clone)]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
+enum Command {
+    /// This message
+    #[command(aliases = ["h", "?"])]
+    Help,
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => {
+            log::info!("Help command");
+            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                .await?
+        }
+    };
+
+    Ok(())
 }
