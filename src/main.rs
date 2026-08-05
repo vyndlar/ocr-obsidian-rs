ed746940cf0311e94c7739485fbb33b5944720b3
rrjackson82 <danonator821@gmail.com>
2026-08-04 23:18:31 -0400

    added other basic commands, no real features yet

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,74 @@ async fn main() {
     Command::repl(bot, answer).await;
 }
 
-// The 'comment' is actually what displays to the user
+// The 'comments' below are actually displayed to the user
 #[derive(BotCommands, Clone)]
 #[command(
     rename_rule = "lowercase",
     description = "These commands are supported:"
 )]
 enum Command {
-    /// This message
+    /// This message, add a command name to get help with a specific command
+    /// Example ````/help ls``` would give help for the 'List' command
     #[command(aliases = ["h", "?"])]
-    Help,
+    Help { help_command: String },
+
+    /// List all vaults
+    #[command(aliases = ["ls"])]
+    List,
+
+    /// Settings Panel
+    #[command()]
+    Settings,
+
+    /// Add a new vault
+    #[command()]
+    New { data: String },
 }
 
 async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
     match cmd {
-        Command::Help => {
+        Command::Help { help_command } => {
             log::info!("Help command");
+            if help_command.is_empty() {
+                bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                    .await?
+            } else {
+                bot.send_message(
+                    msg.chat.id,
+                    get_help_with_command(help_command.to_lowercase().as_str()),
+                )
+                .await?
+            }
+        }
+        Command::List => {
+            log::info!("List Command");
             bot.send_message(msg.chat.id, Command::descriptions().to_string())
                 .await?
+        }
+        Command::Settings => {
+            log::info!("Settings");
+            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                .await?
+        }
+        Command::New { data } => {
+            bot.send_message(
+                msg.chat.id,
+                format!("{}, {}", data, Command::descriptions()),
+            )
+            .await?
         }
     };
 
     Ok(())
+}
+
+fn get_help_with_command(command: &str) -> String {
+    match command {
+        "list" | "ls" => {
+            "The ```list``` function listst all vaults that ocr-obsidian can see. If one you want is not here, try using the ```/add``` command.".to_string()
+        },
+
+        _ => "Error".to_string(),
+    }
 }
